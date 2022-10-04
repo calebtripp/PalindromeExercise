dd070040314a9e620cb189563d3af203b504c12d
calebtripp <calebtripp@outlook.com>
2022-10-04 14:53:11 -0400

    Exercise 2 COmplete

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("madam", true)]
+        [InlineData("panama", false)]
 
+        public void PalindromeTester(string word, bool expected)
+        {
+            var wordsmith = new WordSmith();
+            var actual = wordsmith.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
